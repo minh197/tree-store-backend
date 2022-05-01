2bb4d3958e731299a25757fe8cf51469c76d37df
Minh Nguyen <mnguyen23@wooster.edu>
2022-05-01 10:28:01 -0400

    Fix the json description

--- .keystone/admin/.next/static/chunks/webpack.js
+++ .keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "424303c92c56dfc7903f";
+/******/ 	var hotCurrentHash = "a59c787a6520efd06924";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
